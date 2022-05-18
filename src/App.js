3198945ba2f2baedc3c3acdb010dd5eba99b8391
ansumariamjacob <ansumariam99@gmail.com>
2022-05-18 22:47:56 +0530

    searchCompleted

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Addemployee from './component/Addemployee';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Searchemp from './component/Searchemp';
 
 function App() {
   return (
     <div >
       <Addemployee/>
+      <Searchemp/>
 
     </div>
   );
---
+++ src/component/Searchemp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const Searchemp = () => {
+    var [empid,setId]=useState("")
+    const searchEmp=()=>{
+        const data={"empid":empid}
+        console.log(data)
+    }
+  return (
+    <div>
+        <div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-3">
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Employee id</label>
+                    <input onChange={(e)=>{setId(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button onClick={searchEmp} class="btn btn-success">SEARCH</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default Searchemp
