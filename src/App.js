69a9deec1dd033b5fa705608c49ed49293828b04
ansumariamjacob <ansumariam99@gmail.com>
2022-05-18 22:27:37 +0530

    addCompleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Addemployee from './component/Addemployee';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Addemployee/>
 
     </div>
   );
