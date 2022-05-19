59bbb16486a9fdc798eca58a64f75729de3b3aa7
ansumariamjacob <ansumariam99@gmail.com>
2022-05-19 09:54:26 +0530

    top

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Addemployee from './component/Addemployee';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Searchemp from './component/Searchemp';
+import Header from './component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <Addemployee/>
-      <Searchemp/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Addemployee/>}/>
+        <Route path="/search" exact element={<Searchemp/>}/>
+      </Routes>
+      </BrowserRouter>
 
     </div>
   );
